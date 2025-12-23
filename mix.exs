441c7e631fa8c1c34d9e065383a1fb5ad6bad60e
ausimian <nick@ausimian.net>
2025-12-23 15:35:28 +1100

    Lower min elixir version to 1.16

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Madness.MixProject do
     [
       app: :madness,
       version: "0.1.0",
-      elixir: "~> 1.18",
+      elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
